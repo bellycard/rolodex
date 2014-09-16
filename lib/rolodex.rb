3a1a8571fb440e8c445e8e83cfe1a62e84bb6439
Shay Howe <shayhowe@gmail.com>
2014-09-16 18:03:50 -0500

    Updating sprockets

--- lib/rolodex.rb
+++ lib/rolodex.rb
@@ -3,14 +3,13 @@ require 'rolodex/sass'
 
 module Rolodex
 
+  Angular::Html2js.configure do |config|
+    config.module_name = 'templates'
+    config.init_sprockets
+  end
+  
   if defined?(::Middleman)
     require "middleman-autoprefixer"
-
-    Angular::Html2js.configure do |config|
-      config.module_name = 'templates'
-      config.init_sprockets
-    end
-
   end
 
 end
