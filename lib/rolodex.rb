76b4f7cc123e82d4036dc6130949ac7bf7c3773b
Joe Wegner <joe@wegnerdesign.com>
2015-01-08 11:13:14 -0600

    make it work with rails

--- lib/rolodex.rb
+++ lib/rolodex.rb
@@ -2,10 +2,30 @@ require 'angular/html2js'
 require 'rolodex/sass'
 
 module Rolodex
+  class << self
+    # Inspired by bootstrap-sass
+    def load!
+      Angular::Html2js.configure do |config|
+        config.module_name = 'templates'
+        config.init_sprockets
+      end
 
-  Angular::Html2js.configure do |config|
-    config.module_name = 'templates'
-    config.init_sprockets
+      if defined?(::Rails)
+        require 'rolodex/engine'
+      elsif defined?(:Sprockets)
+        Sprockets.append_path(File.join(asset_path, 'stylesheets'))
+        Sprockets.append_path(File.join(asset_path, 'javascripts'))
+      end
+    end
+
+    def gem_path
+      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+    end
+
+    def asset_path
+      @asset_path ||= File.join gem_path, 'vendor', 'assets'
+    end
   end
-
 end
+
+Rolodex.load!
