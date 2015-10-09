84908ed40c77c2549fc27cfd0b35f0b84283a000
Pete Brousalis <brousapg@gmail.com>
2015-10-09 13:44:22 -0500

    added a blank angular module called templates to keep the build from breaking

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var path = require('path');
 var del = require('del');
+var source = require('vinyl-source-stream');
 var $ = require('gulp-load-plugins')();
 
 var assetPath = 'vendor/assets/javascripts/rolodex_angular';
@@ -10,11 +11,18 @@ var distPath = 'dist';
 var tmpPath = '.tmp';
 
 // Compiles coffee to js, annotates Angular dependencies
-gulp.task('js', function() {
+gulp.task('js', ['js-hack'], function() {
   return gulp.src(path.join(assetPath, '/**/*.coffee'))
     .pipe($.coffee())
     .pipe($.ngAnnotate())
     .pipe(gulp.dest(tmpPath))
+});
+
+// A hack for non-Ruby projects to fake the templates namespace (which lives in a gem)
+gulp.task('js-hack', function() {
+  var stream = source('templates.js');
+  stream.end("(function() { angular.module('templates', []); }).call(this);");
+  stream.pipe(gulp.dest(tmpPath));
 });
 
 // Compiles haml to js for Angular templateCache
