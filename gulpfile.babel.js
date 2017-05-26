3987c514553879cc65501fc5f6ada5f4c409ca90
Rodney Dennis <rodneydennisjr@gmail.com>
2017-05-26 07:21:37 -0500

    [feat ] Gulp (#131)
    * Revert "Gulpify"
    
    This reverts commit 6c92d228537a1d19329d47358ad4aba9ca47f9ee.
    
    * Revert "Revert "Gulpify""
    
    This reverts commit 91fbc71beaee2b8a6463538faa4ba2d4939fc514.
    
    * Build
    
    * 3.1.0
    
    * Build
    
    * 3.1.1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,24 +1,31 @@
 import gulp from 'gulp';
 import rename from 'gulp-rename';
+import header from 'gulp-header';
 import postcss from 'gulp-postcss';
 import postcssImport from 'postcss-import'
 import cssnano from 'cssnano';
 import autoprefixer from 'autoprefixer';
+import pkg from './package.json';
 
-gulp.task('css', () => {
+const banner = `/*! ${pkg.name} v${pkg.version} | ${pkg.license} License | ${pkg.repository.url} */\n`;
+
+gulp.task('default', () => {
   return gulp.src('./src/rolodex.css')
     .pipe(postcss([
       postcssImport(),
-      autoprefixer(),
+      autoprefixer()
+    ]))
+    .pipe(header(banner, { pkg : pkg } ))
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(postcss([
+      cssnano({
+        svgo: false,
+      })
     ]))
     .pipe(gulp.dest('./dist'))
-    .pipe(rename({
-      extname: '.min.css',
-    }))
-    .pipe(postcss([
-      cssnano({svgo: false}),
-    ]))
-    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('watch', ['default'], () => {
+  gulp.watch('src/**/*.css', ['default'])
+});
