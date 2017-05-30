97ffd1404bfcd90873917a9b98b7a7173934d597
Rodney Dennis <rodneydennisjr@gmail.com>
2017-05-30 15:39:30 -0500

    Compile CSS root variables

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,8 +3,8 @@ import rename from 'gulp-rename';
 import header from 'gulp-header';
 import postcss from 'gulp-postcss';
 import postcssImport from 'postcss-import'
+import postcssCssnext from 'postcss-cssnext';
 import cssnano from 'cssnano';
-import autoprefixer from 'autoprefixer';
 import pkg from './package.json';
 
 const banner = `/*! ${pkg.name} v${pkg.version} | ${pkg.license} License | ${pkg.repository.url} */\n`;
@@ -13,14 +13,14 @@ gulp.task('default', () => {
   return gulp.src('./src/rolodex.css')
     .pipe(postcss([
       postcssImport(),
-      autoprefixer()
+      postcssCssnext(),
     ]))
     .pipe(header(banner, { pkg : pkg } ))
     .pipe(gulp.dest('./dist'))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(postcss([
       cssnano({
-        svgo: false,
+        autoprefixer: false,
       })
     ]))
     .pipe(gulp.dest('./dist'))
